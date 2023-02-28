75cb976ed8cb7d178631a313699884c445d44f07
Ashu81 <ashusharma2277@gmail.com>
2023-03-01 00:21:00 +0530

    User Authentication and Authorization with token

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,35 +1,11 @@
 import express from "express";
-import Product from "../Models/productModel.js";
-import asyncHandler from "express-async-handler";
+import {
+  getProductById,
+  getProducts,
+} from "../controller/productController.js";
 const router = express.Router();
 
-// @desc     Fetch all Productsrs
-// @desc     GET /api/products
-// @desc     access public
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-
-    res.json(products);
-  })
-);
-
-// @desc     Fetch single Product
-// @desc     GET /api/products/:id
-// @desc     access public
-
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product not Found");
-    }
-  })
-);
 export default router;
